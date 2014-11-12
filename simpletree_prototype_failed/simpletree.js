75188dd06ff15160a036881cf6acc7ea8b9bc907
jiadongy <jiadongy@163.com>
2014-11-13 00:17:54 +0800

    2014.11.12

--- simpletree_prototype_failed/simpletree.js
+++ simpletree_prototype_failed/simpletree.js
@@ -13,7 +13,7 @@
  *      如在addAssign()判断方法是否存在是用hasOwnPrototpe而不是prop in scopr，编程起来不直观
  * 2.事件触发没法改变上下文的this，如单击时传入的liveClick(this..)是HTML Element，
  *      因此没法在liveClick内部获得vm的属性和方法（这个没法解决，除非将其重新移入VM中）
- * 
+ *
  * 2014.11.10 at RTIO Lab
  */
 "use strict";
